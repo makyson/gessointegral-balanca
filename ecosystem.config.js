8e60b0409a980d57098488d29bb707fcd231f68d
Iosley Carlos Silva <iosley@outlook.com>
2018-09-06 00:33:15 -0300

    Ajusta caminho da pasta do usuário no deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,12 @@ module.exports = {
   }],
   deploy: {
     production: {
-      key: '~/.ssh/iosley.pem',
+      key: '$HOME/.ssh/iosley.pem',
       user: 'seges',
       host: [{ host: '177.67.95.238 -p 422' }],
       ref: 'origin/master',
       repo: 'git@github.com:Username/repository.git',
-      path: '~/balanca',
+      path: '$HOME/balanca',
       'post-deploy': 'npm i --no-save && pm2 restart balanca',
     },
   },
